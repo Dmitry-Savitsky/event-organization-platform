c530c2eb28116f1218bbc43838608defc2d4d1b0
Dmitry-Savitsky <dmsw0rk333@gmail.com>
2023-12-13 17:05:10 +0300

    feat: + routers

--- server/index.js
+++ server/index.js
@@ -1,14 +1,21 @@
 require(`dotenv`).config()
 
 const express = require("express")
-
 const sequelize = require(`./db`)
-
 const models = require(`./models/models`)
+const cors = require(`cors`)
+const { json } = require("sequelize")
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
+app.use(cors())
+app.use(express.json())
+
+// app.get(`/`, (req, res) => {
+//     res.status(200).json({massage: `working . . . `})
+// })
+
 
 const start = async() => {
     try {
